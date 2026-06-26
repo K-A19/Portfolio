e860903651f6e65f57906944c1fdc5665d9d61bc
K-A19 <kamiyeafolabi@hotmail.com>
2026-06-26 11:24:53 -0400

    Set up class state for the scrapbook - Applying different states if the book is open   or if it is closed and showing one of the two   covers

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css'
 import HTMLFlipBook from 'react-pageflip'
+import { useState } from 'react'
 import Cover from './pages/Cover'
 import BackCover from './pages/BackCover'
 import Page from './pages/Page'
 
 function App() {
 
+  const [currentPage, setCurrentPage] = useState(0)
+
   return (
-    <div id="scrapbook">
-      <HTMLFlipBook width={550} height={500} showCover="true">
+    <div id="scrapbook" className={currentPage === 0 ? 'closed' : 'open'}>
+      <HTMLFlipBook width={550} height={500} showCover="true" onFlip={(e) => setCurrentPage(e.data)}>
       <Cover />
       <Page />
       <Page />
