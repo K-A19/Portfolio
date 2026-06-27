66a7a30247cd7ee2b837b044942119dda62d3670
K-A19 <kamiyeafolabi@hotmail.com>
2026-06-27 00:53:56 -0400

    Inserted university logos on the book cover - The rough placements of the logos have been   established - Space for visual 'tapping' has been been added   to each logo image at the top

--- src/pages/Cover.jsx
+++ src/pages/Cover.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import './Cover.css';
 import hackphoto from '../assets/hackathon_photo.jpg';
+import uwlogo from '../assets/waterloo_logo.png';
+import laurierlogo from '../assets/laurier_logo.png';
+
 const Cover = React.forwardRef((props, ref) => {
   return (
     <div className="cover" ref={ref}>
@@ -8,6 +11,8 @@ const Cover = React.forwardRef((props, ref) => {
       <div className="strip" id="namestrip"> &emsp;Kami Afolabi&emsp; </div>
       <div className="strip" id="degreestrip">&emsp;UW / WLU Double Degree Student&emsp;</div>
       <div className="strip" id="portfoliostrip">&emsp;My Portfolio&emsp;</div>
+      <img id="uwlogo" className="logo" src={uwlogo} alt="University of Waterloo's Logo" />
+      <img id="laurierlogo" className="logo" src={laurierlogo} alt="Wilfrid Laurier University's Logo" />
     </div>
   );
 });
