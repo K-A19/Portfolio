be25569b9dd2654fdcdc19c3374249e44a98a081
K-A19 <kamiyeafolabi@hotmail.com>
2026-06-27 00:04:42 -0400

    Included editing for the polaroid cover photo

--- src/pages/Cover.jsx
+++ src/pages/Cover.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import './Cover.css';
 import hackphoto from '../assets/hackathon_photo.jpg';
-
 const Cover = React.forwardRef((props, ref) => {
   return (
     <div className="cover" ref={ref}>
-      <div id="photoframe"><img id="coverphoto" src={hackphoto} alt="Kami Afolabi" /></div>
+      <img id="coverphoto" src={hackphoto} alt="Kami Afolabi" />
       <div id="namestrip"> Kami Afolabi </div>
     </div>
   );
