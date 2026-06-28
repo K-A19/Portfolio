c398e09cb923885ad77dc0f1e1241b43f861617d
K-A19 <kamiyeafolabi@hotmail.com>
2026-06-27 21:16:33 -0400

    Added a caption to the cover photo

--- src/pages/Cover.jsx
+++ src/pages/Cover.jsx
@@ -9,6 +9,7 @@ const Cover = React.forwardRef((props, ref) => {
     <div className="cover" ref={ref}>
       <div id="spine"></div>
       <img id="coverphoto" src={hackphoto} alt="Kami Afolabi" />
+      <div id="photocaption">hackathon '26</div>
       <div className="strip" id="namestrip"> &emsp;Kami Afolabi&emsp; </div>
       <div className="strip" id="degreestrip">&emsp;UW / WLU Double Degree Student&emsp;</div>
       <div className="strip" id="portfoliostrip">&emsp;My Portfolio&emsp;</div>
